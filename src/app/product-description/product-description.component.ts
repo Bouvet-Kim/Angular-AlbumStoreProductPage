a36f383b35dd68e050f9d2e29bc6436ad0144cee
Kim Rønvik <Kim.Ronvik@bouvet.no>
2020-10-22 11:48:13 +0200

    Part 4

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -7,8 +8,8 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
-  
+  albumInfo: Album;
+
   constructor(private _productService: ProductService) { }
 
   ngOnInit(): void {
